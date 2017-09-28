41c325fc92b6fef950ec2bbd3f081a543776af26
Gabriel Osborne <osbornegabriel@gmail.com>
2017-09-28 16:52:07 -0500

    Add second function to challenge number two, due to question ambiguity. One now console logs your current window, the other calls the print command

--- js_basic_80_challenges/challenge_2.js
+++ js_basic_80_challenges/challenge_2.js
@@ -1,4 +1,8 @@
 // Write a JavaScript program to print the contents of the current window.  Go to the editor
-var windowPrint = function(){
+var windowLog = function(){
   console.log(window);
+}
+
+var windowPrint = function(){
+  window.print();
 }
