05b5699c6dc37e52d7d32c2c879de1bc015aef73
Gabriel Osborne <osbornegabriel@gmail.com>
2017-11-28 14:59:46 -0600

    Write remove character function

---
+++ js_basic_80_challenges/challenge_22.js
@@ -0,0 +1,5 @@
+// 22. Write a JavaScript program to remove a character at the specified position of a given string and return the new string.
+
+var removeChar = function(string, index){
+  return string.slice(0,index) + string.slice(index + 1, string.length);
+}
